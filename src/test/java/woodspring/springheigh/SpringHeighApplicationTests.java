dcfe6f9bd8fd62762b5fdcf6f38f354c595fa179
woodspring.toronto <woodspring.toronto@gmail.com>
2019-04-07 10:22:05 -0400

    test main

--- src/test/java/woodspring/springheigh/SpringHeighApplicationTests.java
+++ src/test/java/woodspring/springheigh/SpringHeighApplicationTests.java
@@ -1,4 +1,4 @@
-package woodspring.springheight;
+package woodspring.springheigh;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringHeightApplicationTests {
+public class SpringHeighApplicationTests {
 
 	@Test
 	public void contextLoads() {
